c856e37cd79eb0fa8139621d50ac75930c57b5f8
Atosh Mohonto <135542605+AtoshMohonto@users.noreply.github.com>
2024-11-22 00:56:21 +0800

    php basic
    php kuss dada class basic with dayal and rony

---
+++ atosh/index.php
@@ -0,0 +1,24 @@
+hi atosh 
+<form action="">
+    <input type="text" placeholder="Name" required > 
+    <input type="NUMBER" value="124"> 
+    <input type="text" value="Atosh"<?php 
+     $user="atosh2";
+    if($user=="atosh"){
+        echo "readonly";
+    }
+    ?> > 
+    <input type="HIDDEN"> 
+    <input type="SUBMIT"> 
+</form>
+<?php
+echo'           
+
+d<input type="text" placeholder="Name" required > 
+<p onClick="window.location.href='www.'"> nobiuyf</p>
+
+'
+
+;
+
+?>
